6f52dc9a674be86fc7082b99a578f114d78b6f77
Rui Chen <chen107101@gmail.com>
2023-04-23 17:40:46 -0700

    add model and training result

--- model/predict.py
+++ model/predict.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 import pickle as pkl
 
 if __name__ == "__main__":
-    model = tf.keras.models.load_model("./mymodel_25")
+    model = tf.keras.models.load_model("./categorical_crossentropy_onehot_15")
 
-    with open('./dataset_train.pkl', "rb") as f:
+    with open('./dataset_test_onehot.pkl', "rb") as f:
         [X_test, y_test] = pkl.load(f)
     print(X_test[0].shape)
-    output = model.predict(X_test[0].reshape(512,77))
-    print(output)
+    loss, top1, top3, top5, top10 = model.evaluate(X_test, y_test)
+    print(loss, top1, top3, top5, top10)
 
     # loss, top1, top3, top5, top10 = model.evaluate(X_test, y_test)
--- model/train.py
+++ model/train.py
@@ -20,9 +20,9 @@ def draw(val, history, loss):
 
 
 if __name__ == "__main__":
-    with open('./dataset_train_onehot_without_position.pkl', "rb") as f:
+    with open('./dataset_train_onehot.pkl', "rb") as f:
         [X_train, y_train] = pkl.load(f)
-    with open('./dataset_test_onehot_without_position.pkl', "rb") as f:
+    with open('./dataset_test_onehot.pkl', "rb") as f:
         [X_test, y_test] = pkl.load(f)
     print(X_train.shape)
     print(y_train.shape)
@@ -30,10 +30,10 @@ if __name__ == "__main__":
     print(y_test.shape)
 
     model = keras.Sequential()
-    model.add(layers.LSTM(units = 128, input_shape = (X_train.shape[1], X_train.shape[2])))
+    model.add(layers.SimpleRNN(units = 128, input_shape = (X_train.shape[1], X_train.shape[2])))
     model.add(layers.Dense(units = y_test.shape[1], activation = 'softmax'))
     print("model constructed")
-    loss = 'mean_squared_error'
+    loss = 'categorical_crossentropy'
     model.compile(optimizer = 'adam', loss = loss,
                   metrics = [tf.keras.metrics.TopKCategoricalAccuracy(k = 1, name = "top-1"),
                              tf.keras.metrics.TopKCategoricalAccuracy(k = 3, name = "top-3"),
@@ -42,7 +42,7 @@ if __name__ == "__main__":
 
     callbacks = [
         keras.callbacks.ModelCheckpoint(
-            filepath = loss+"_onehot_without_position_{epoch}",
+            filepath = loss+"_simplernn_onehot_{epoch}",
             save_best_only = True,  # Only save a model if `val_loss` has improved.
             monitor = "val_loss",
             verbose = 1,
@@ -55,8 +55,8 @@ if __name__ == "__main__":
     print('Test loss:', test_loss)
     print('Test accuracy:', top1, top3, top5, top10)
 
-    draw('loss', history, loss+"_onehot_without_position")
-    draw('top-1', history, loss+"_onehot_without_position")
-    draw('top-3', history, loss+"_onehot_without_position")
-    draw('top-5', history, loss+"_onehot_without_position")
-    draw('top-10', history, loss+"_onehot_without_position")
+    draw('loss', history, loss+"_simplernn_onehot")
+    draw('top-1', history, loss+"_simplernn_onehot")
+    draw('top-3', history, loss+"_simplernn_onehot")
+    draw('top-5', history, loss+"_simplernn_onehot")
+    draw('top-10', history, loss+"_simplernn_onehot")
